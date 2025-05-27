94e0099b20c139f0ee5ea19fb253ee193974a640
Rubai Rahman <rubairahman1@gmail.com>
2025-05-27 18:35:15 +0600

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { decrement, increment } from '@/redux/features/counter/counterSlice';
-import { Button } from '@/components/ui/button';
-import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
-import Home from '@/pages/Home/Home';
+import MainLayout from '@/components/layouts/MainLayout';
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
-
   return (
     <>
-      <div className="flex items-center justify-center bg-red-600">
-        <Button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <span className="font-semibold text-8xl">{count}</span>
-        <Button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </Button>
-      </div>
-      <Home />
+      <MainLayout />
     </>
   );
 }
