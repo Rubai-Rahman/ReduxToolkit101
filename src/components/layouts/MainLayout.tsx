4908f4635e633cb9afb7cb20650740adc95e81d7
Rubai Rahman <rubairahman1@gmail.com>
2025-06-24 23:38:11 +0600

    layout updated

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const MainLayout = () => {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <main className="flex-1 overflow-auto pt-[60px]">
+      <main className="">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
