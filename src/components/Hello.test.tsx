c5f9336f101a8dbaa6c114c7924608de8e8407fe
Rubai Rahman <rubairahman1@gmail.com>
2025-06-29 00:31:31 +0600

    (not “Commit & Push”)

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -4,7 +4,7 @@ function Hello() {
   return <h1>Hello World</h1>;
 }
 
-describe('Hello component', () => {
+describe('Hello component4', () => {
   it('render correctly again', () => {
     render(<Hello />);
     expect(screen.getByText('Hello World')).toBeInTheDocument();
