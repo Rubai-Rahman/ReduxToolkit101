36611c1ab5d290406b5e4addced2681076ed4e53
Rubai Rahman <rubairahman1@gmail.com>
2025-06-29 00:06:22 +0600

    test

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 
 function Hello() {
   return <h1>Hello World</h1>;
 }
 
-describe("Hello component", () => {
-  it("render correctly", () => {
+describe('Hello component', () => {
+  it('render correctly again', () => {
     render(<Hello />);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
   });
 });
