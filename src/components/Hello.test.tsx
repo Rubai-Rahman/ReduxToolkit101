09251dacc6acba1857354da7b462116a6a4dbd80
Rubai Rahman <rubairahman1@gmail.com>
2025-06-29 23:18:23 +0600

    check test

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -4,7 +4,7 @@ function Hello() {
   return <h1>Hello World</h1>;
 }
 
-describe('Hello componen', () => {
+describe('Hello component', () => {
   it('render correctly', () => {
     render(<Hello />);
     expect(screen.getByText('Hello World')).toBeInTheDocument();
